b6614b4c5d78a6e5468e25df5c7615e830c5164b
gyucode <gyucode@gmail.com>
2019-12-15 11:05:54 +0900

    백버튼을 연속으로 눌러야 앱이 종료되도록 변경

--- app/src/main/java/com/example/a20191215_01_lifecycle/MainActivity.kt
+++ app/src/main/java/com/example/a20191215_01_lifecycle/MainActivity.kt
@@ -4,9 +4,12 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity() {
+
+    var lastBackButtonTime = 0L
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,6 +24,24 @@ class MainActivity : BaseActivity() {
             val intent = Intent(mContext,SecondActivity::class.java)
             startActivity(intent)
         }
+
+    }
+
+    override fun onBackPressed() {
+//        super.onBackPressed()
+        val currentTime = System.currentTimeMillis()
+
+//        현재 시간 기준으로 1초 이내에 백버튼을 누른 기록이 있다면 종료
+//        [간격]이 [1초 1000ms]가 넘어가면 토스트
+        if(currentTime - lastBackButtonTime > 1000){
+            Toast.makeText(mContext,"한번 더 뒤로 버튼을 누르면 종료됩니다",Toast.LENGTH_SHORT).show()
+        }
+        else{
+            finish()
+        }
+
+//        마지막으로 누른 시간을 갱신시켜줌
+        lastBackButtonTime = currentTime
 
     }
 
